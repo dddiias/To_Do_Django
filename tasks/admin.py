ca383bb493d1f909a3d0c3b70ce1b6532e3b3538
Dias Salakhutdinov <dias.salakhutdinov@mail.ru>
2025-08-11 12:07:01 +0500

    feat(tasks): add TaskSerializer binding owner from request

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "owner", "status", "created_at", "due_date")
+    list_filter = ("status", "created_at")
+    search_fields = ("title", "description")
--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Task
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
@@ -16,3 +17,13 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.set_password(validated_data["password"])
         user.save()
         return user
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ("id", "title", "description", "status", "created_at", "due_date")
+        read_only_fields = ("id", "created_at")
+
+    def create(self, validated_data):
+        user = self.context["request"].user
+        return Task.objects.create(owner=user, **validated_data)
